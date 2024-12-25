d0548e6677e6dd90c7ecfbf06da0585d044f361e
viviint <96400522+viviint@users.noreply.github.com>
2024-12-25 16:15:07 +0800

    Add Layout and includes element

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,106 @@
+<div class="clearfix colelem" id="pu17959"><!-- group -->
+
+    <div class="verticalspacer"></div>
+    <div class="browser_width colelem" id="u17154-bw">
+        <div id="u17154"><!-- group -->
+            <div class="clearfix" id="u17154_align_to_page">
+                <div class="clearfix grpelem" id="pu17172-4"><!-- column -->
+                    <div class="clearfix colelem" id="u17172-4"><!-- content -->
+                        <p>About Us</p>
+                    </div>
+                    <div class="clearfix colelem" id="u17156-4"><!-- content -->
+                        <p>Healthtix, we are committed to providing reliable, clear, and accessible
+                            information on various diseases and health conditions from around the world. Our
+                            team of experts delivers up-to-date, evidence-based content to help you
+                            understand illnesses, their symptoms, treatments, and preventive measures, so
+                            you can make informed decisions about your health and well-being</p>
+                    </div>
+                </div>
+                <div class="clearfix grpelem" id="pu17162-4"><!-- column -->
+                    <div class="clearfix colelem" id="u17162-4"><!-- content -->
+                        <p>Our Info</p>
+                    </div>
+                    <div class="clearfix colelem" id="pu22429"><!-- group -->
+                        <div class="grpelem" id="u22429"><!-- custom html -->
+                            <i class="fa fa-map-marker"></i>
+                        </div>
+                        <div class="clearfix grpelem" id="u17158-4"><!-- content -->
+                            <p>999&nbsp; Street Ca 70079</p>
+                        </div>
+                    </div>
+                    <div class="clearfix colelem" id="pu22431"><!-- group -->
+                        <div class="grpelem" id="u22431"><!-- custom html -->
+                            <i class="fa fa-phone "></i>
+                        </div>
+                        <div class="clearfix grpelem" id="u17159-4"><!-- content -->
+                            <p>1-811-234-5678</p>
+                        </div>
+                    </div>
+                    <div class="clearfix colelem" id="pu22433"><!-- group -->
+                        <div class="grpelem" id="u22433"><!-- custom html -->
+                            <i class="fa fa-fax "></i>
+                        </div>
+                        <div class="clearfix grpelem" id="u17160-4"><!-- content -->
+                            <p>1-811-234-5679</p>
+                        </div>
+                    </div>
+                    <div class="clearfix colelem" id="pu22435"><!-- group -->
+                        <div class="grpelem" id="u22435"><!-- custom html -->
+                            <i class="fa fa-envelope"></i>
+                        </div>
+                        <div class="clearfix grpelem" id="u17161-4"><!-- content -->
+                            <p>kelompok4@mail.com</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="browser_width colelem" id="u17155-bw">
+        <div id="u17155"><!-- group -->
+            <div class="clearfix" id="u17155_align_to_page">
+                <div class="clip_frame grpelem" id="u17182"><!-- image -->
+                    <img class="block" id="u17182_img" src="{{ asset('storage/images/heart2886.png') }}" alt=""
+                        width="29" height="26" />
+                </div>
+                <div class="clearfix grpelem" id="u17181-5"><!-- content -->
+                    <p><span id="u17181">H</span>EALTHTIX</p>
+                </div>
+                <div class="clearfix grpelem" id="u17163-5"><!-- content -->
+                    <p id="u17163-2">Copyright © 2024 Kelompok 4.</p>
+                    <p id="u17163-3">&nbsp;</p>
+                </div>
+                <div class="grpelem" id="u17322"><!-- custom html -->
+                    <script>
+                        $('.fadeInUp').addClass('wow fadeInUp');
+                        $('.fadeInDown').addClass('wow fadeInDown');
+                        $('.fadeInLeft').addClass('wow fadeInLeft');
+                        $('.fadeInRight').addClass('wow fadeInRight');
+                        $('.fadeIn').addClass('wow fadeIn');
+                        $('.zoomIn').addClass('wow zoomIn');
+                    </script>
+
+                    <script>
+                        new WOW().init();
+                    </script>
+
+                </div>
+                <div class="grpelem" id="u22268"><!-- custom html -->
+                    <i class="fa fa-facebook"></i>
+                </div>
+                <div class="grpelem" id="u22270"><!-- custom html -->
+                    <i class="fa fa-instagram"></i>
+                </div>
+                <div class="grpelem" id="u22272"><!-- custom html -->
+                    <i class="fa fa-google-plus"></i>
+                </div>
+                <div class="grpelem" id="u22274"><!-- custom html -->
+                    <i class="fa fa-twitter"></i>
+                </div>
+                <div class="grpelem" id="u22276"><!-- custom html -->
+                    <i class="fa fa-pinterest"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,106 @@
+<div class="browser_width" id="u22267-bw">
+    <div class="pinned-colelem" id="u22267"><!-- simple frame --></div>
+</div>
+<a class="nonblock nontext Button anim_swing rounded-corners clearfix pinned-colelem" id="buttonu17284"
+    href="{{ route('login') }}"><!-- container box -->
+    <div class="clearfix grpelem" id="u17285-4">
+        <!-- content -->
+        <p>Login Or Register</p>
+    </div>
+</a>
+<div class="clearfix pinned-colelem" id="u17377">
+    <!-- group -->
+    <div class="clip_frame grpelem" id="u17287">
+        <!-- image -->
+        <img class="block" id="u17287_img" src="{{ asset('storage/images/heart2886.png') }}" alt=""
+            width="35" height="31" />
+    </div>
+    <div class="clearfix grpelem" id="u17283-5">
+        <!-- content -->
+        <p><span id="u17283">H</span>EALTHTIX</p>
+    </div>
+</div>
+<div class="infocontact">
+    <div class="pinned-colelem" id="u22249">
+        <!-- custom html -->
+        <i class="fa fa-phone"></i>
+    </div>
+    <div class="clearfix pinned-colelem" id="u17220-4">
+        <!-- content -->
+        <p>1-811-234-5678</p>
+    </div>
+    <div class="pinned-colelem" id="u22251">
+        <!-- custom html -->
+        <i class="fa fa-fax"></i>
+    </div>
+    <div class="clearfix pinned-colelem" id="u17221-4">
+        <!-- content -->
+        <p>1-811-234-5679</p>
+    </div>
+    <div class="pinned-colelem" id="u22253">
+        <!-- custom html -->
+        <i class="fa fa-envelope"></i>
+    </div>
+    <div class="clearfix pinned-colelem" id="u17222-4">
+        <!-- content -->
+        <p>info@mail.com</p>
+    </div>
+</div>
+<div class="navber">
+    <div class="browser_width" id="u17226-bw">
+        <div class="pinned-colelem" id="u17226"><!-- simple frame --></div>
+    </div>
+    <nav class="MenuBar clearfix pinned-colelem" id="menuu17227">
+        <!-- horizontal box -->
+        <div class="MenuItemContainer clearfix grpelem" id="u17228">
+            <!-- vertical box -->
+            <a class="nonblock nontext MenuItem MenuItemWithSubMenu {{ Request::is('/') ? 'MuseMenuActive' : '' }} clearfix colelem"
+                id="u17229" href="{{ url('/') }}"><!-- horizontal box -->
+                <div class="MenuItemLabel NoWrap clearfix grpelem" id="u17230-4">
+                    <!-- content -->
+                    <p>Home</p>
+                </div>
+            </a>
+        </div>
+        <div class="MenuItemContainer clearfix grpelem" id="u17270">
+            <!-- vertical box -->
+            <a class="nonblock nontext MenuItem MenuItemWithSubMenu {{ Request::is('about') ? 'MuseMenuActive' : '' }} clearfix colelem"
+                id="u17271" href="{{ url('about') }}"><!-- horizontal box -->
+                <div class="MenuItemLabel NoWrap clearfix grpelem" id="u17274-4">
+                    <!-- content -->
+                    <p>About</p>
+                </div>
+            </a>
+        </div>
+        <div class="MenuItemContainer clearfix grpelem" id="u17263">
+            <!-- vertical box -->
+            <a class="nonblock nontext MenuItem MenuItemWithSubMenu clearfix colelem" id="u17264"
+                href="articles.html"><!-- horizontal box -->
+                <div class="MenuItemLabel NoWrap clearfix grpelem" id="u17267-4">
+                    <!-- content -->
+                    <p>Article</p>
+                </div>
+            </a>
+        </div>
+    </nav>
+</div>
+<div class="pinned-colelem" id="u22257">
+    <!-- custom html -->
+    <i class="fa fa-facebook"></i>
+</div>
+<div class="pinned-colelem" id="u22259">
+    <!-- custom html -->
+    <i class="fa fa-instagram"></i>
+</div>
+<div class="pinned-colelem" id="u22261">
+    <!-- custom html -->
+    <i class="fa fa-google-plus"></i>
+</div>
+<div class="pinned-colelem" id="u22263">
+    <!-- custom html -->
+    <i class="fa fa-twitter"></i>
+</div>
+<div class="pinned-colelem" id="u22265">
+    <!-- custom html -->
+    <i class="fa fa-pinterest"></i>
+</div>
